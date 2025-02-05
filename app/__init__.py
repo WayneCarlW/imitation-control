84133f2abd28cc4b80f74c6295f760a59c08a517
WayneCarlW <waynecarlgeno@gmail.com>
2025-02-05 09:33:03 +0300

    Authentication Blueprint setup

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from config import Config
+from app.blueprints.auth import auth_bp
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+
+    #Registration of blueprints
+    app.register_blueprint(auth_bp)
+
+    return app
---
+++ app/blueprints/auth/routes.py
@@ -0,0 +1,13 @@
+from flask import render_template, redirect, url_for, request
+from . import auth_bp
+
+@auth_bp.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        #LOGIN LOGIC
+        return redirect(url_for("dashboard.dashboard_home"))
+    return render_template("auth/login.html"))
+
+@auth_bp.route("/logout")
+def logout():
+    return redirect(url_for("auth.login"))

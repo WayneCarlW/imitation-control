0ba4647bf007835dd337c6ed68d3f812a5d2a038
WayneCarlW <waynecarlgeno@gmail.com>
2025-02-05 11:42:31 +0300

    mongo setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+from config.config import config
+from app.extensions import mongo
 from app.blueprints.auth import auth_bp
 
-def create_app():
+def create_app(config_name="development"):
+    load_dotenv()
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(config[config_name])
 
 
     #Registration of blueprints
